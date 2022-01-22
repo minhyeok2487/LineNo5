2e8ac76edfdbaf90a233a4469e756540925fbd3e
Minhyeok1995 <repeater2487@naver.com>
2022-01-22 16:00:52 +0900

    wav 파일 재생

---
+++ src/youtube/WavPlayer.java
@@ -0,0 +1,39 @@
+package youtube;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.sound.sampled.*;
+
+public class WavPlayer {
+
+	public static void main(String[] args) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+		Scanner scanner = new Scanner(System.in);
+		
+		String filename = "By the Fireplace.wav";
+		File file = new File(filename);
+		AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+		Clip clip = AudioSystem.getClip();
+		clip.open(audioStream);
+		String response = "";
+		while(!response.equals("Q")) {
+			System.out.println("P = Play, S = Stop, R = Reset, Q = Quit");
+			System.out.print("Enter yout choice: ");
+			response = scanner.next();
+			response = response.toUpperCase();
+			switch (response) {
+			case ("P"): clip.start();
+			break;
+			case ("S"): clip.stop();
+			break;
+			case ("R"): clip.setMicrosecondPosition(0);
+			break;
+			case ("Q"): clip.close();
+			break;
+			default : System.out.println("Not a valid response");
+			}
+		}
+		System.out.println("BYE");
+	}
+}
