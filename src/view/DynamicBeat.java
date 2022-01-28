afd6f2801c7503fcdac81137ef64821e0873d2d0
Minhyeok1995 <repeater2487@naver.com>
2022-01-28 10:54:33 +0900

    버튼음 추가

--- src/view/DynamicBeat.java
+++ src/view/DynamicBeat.java
@@ -9,22 +9,29 @@ import java.awt.event.MouseAdapter;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.event.*;
 
 public class DynamicBeat extends JFrame {
 
 	private Image screenImage;
 	private Graphics screenGraphic;
 
-
 	private ImageIcon PlayBtn = new ImageIcon(Main.class.getResource("PlayBtn.png"));
 	private ImageIcon PlayBtnMouseOver = new ImageIcon(Main.class.getResource("PlayBtnMouseOver.png"));
 	private ImageIcon QuitBtn = new ImageIcon(Main.class.getResource("QuitBtn.png"));
 	private ImageIcon QuitBtnMouseOver = new ImageIcon(Main.class.getResource("QuitBtnMouseOver.png"));
+	private ImageIcon ExitBtn = new ImageIcon(Main.class.getResource("ExitButton.png"));
+	private ImageIcon ExitBtnMouseOver = new ImageIcon(Main.class.getResource("ExitButtonMouseOver.png"));
 
 	private Image background = new ImageIcon(Main.class.getResource("introBackground.jpg")).getImage();
 
 	private JButton playButton = new JButton(PlayBtn);
 	private JButton QuitButton = new JButton(QuitBtn);
+
+	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("MenuBar.png")));
+	private JButton exitButton = new JButton(ExitBtn);
+	private int mouseX, mouseY;
 
 	public DynamicBeat() {
 		setUndecorated(true);
@@ -37,31 +44,89 @@ public class DynamicBeat extends JFrame {
 		setBackground(new Color(0, 0, 0, 0));
 		setLayout(null);
 
+		exitButton.setBounds(1245, 0, 30, 30);
+		exitButton.setBorderPainted(false);
+		exitButton.setContentAreaFilled(false);
+		exitButton.setFocusPainted(false);
+		exitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Music buttonSound = new Music("ButtonSound.mp3",false);
+				buttonSound.start();
+				try {
+					Thread.sleep(100);
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+				System.exit(0);
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				exitButton.setIcon(ExitBtnMouseOver);
+				exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonSound = new Music("ButtonSound.mp3",false);
+				buttonSound.start();
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				exitButton.setIcon(ExitBtn);
+				exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+		});
+		add(exitButton);
+		
+		menuBar.setBounds(0, 0, 1280, 30);
+		menuBar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				mouseX = e.getX();
+				mouseY = e.getY();
+			}
+		});
+		menuBar.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				int x = e.getXOnScreen();
+				int y = e.getYOnScreen();
+				setLocation(x - mouseX, y - mouseY);
+			}
+		});
+		add(menuBar);
+
+		
+
 		playButton.setBounds(240, 500, 400, 100);
 		playButton.setBorderPainted(false);
 		playButton.setContentAreaFilled(false);
 		playButton.setFocusPainted(false);
 		playButton.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mousePressed(java.awt.event.MouseEvent e) {
+			public void mousePressed(MouseEvent e) {
+				Music buttonSound = new Music("ButtonSound.mp3",false);
+				buttonSound.start();
 				try {
 					playButton.setVisible(false);
 					QuitButton.setVisible(false);
 					background = new ImageIcon(Main.class.getResource("playBackground.jpg")).getImage();
-					
+
 				} catch (Exception ex) {
 					ex.printStackTrace();
 				}
 			}
 
 			@Override
-			public void mouseEntered(java.awt.event.MouseEvent e) {
+			public void mouseEntered(MouseEvent e) {
+				Music buttonSound = new Music("ButtonSound.mp3",false);
+				buttonSound.start();
 				playButton.setIcon(PlayBtnMouseOver);
 				playButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
 
 			@Override
-			public void mouseExited(java.awt.event.MouseEvent e) {
+			public void mouseExited(MouseEvent e) {
 				playButton.setIcon(PlayBtn);
 				playButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
@@ -74,22 +139,27 @@ public class DynamicBeat extends JFrame {
 		QuitButton.setFocusPainted(false);
 		QuitButton.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mousePressed(java.awt.event.MouseEvent e) {
+			public void mousePressed(MouseEvent e) {
+				Music buttonSound = new Music("ButtonSound.mp3",false);
+				buttonSound.start();
 				try {
-					System.exit(0);
+					Thread.sleep(100);
 				} catch (Exception ex) {
 					ex.printStackTrace();
 				}
+				System.exit(0);
 			}
 
 			@Override
-			public void mouseEntered(java.awt.event.MouseEvent e) {
+			public void mouseEntered(MouseEvent e) {
+				Music buttonSound = new Music("ButtonSound.mp3",false);
+				buttonSound.start();
 				QuitButton.setIcon(QuitBtnMouseOver);
 				QuitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
 
 			@Override
-			public void mouseExited(java.awt.event.MouseEvent e) {
+			public void mouseExited(MouseEvent e) {
 				QuitButton.setIcon(QuitBtn);
 				QuitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
