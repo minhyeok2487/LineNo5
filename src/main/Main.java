02fa15a64a8ebe671aff9e040d8f6d31ed5d3aa5
Minhyeok1995 <repeater2487@naver.com>
2022-02-22 09:21:33 +0900

    로그인 기능 삭제

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,20 +5,11 @@ import java.net.UnknownHostException;
 import javax.swing.JFrame;
 
 import view.MainFrame;
-import view.login.LoginView;
 
 public class Main {
-	LoginView loginView;
 	public static JFrame MA;
-	public static void main(String[] args) {
-		Main main = new Main();
-		main.loginView = new LoginView(); // 로그인창 보이기
-		main.loginView.setMain(main); // 로그인창에게 메인 클래스보내기
-	}
-
-	// 테스트프레임창
-	public void showFrameTest() throws UnknownHostException { 
-		loginView.dispose(); // 로그인창닫기
+	public static void main(String[] args) throws UnknownHostException {
 		MA = new MainFrame(); // 클라이언트 게임 실행
 	}
+
 }
